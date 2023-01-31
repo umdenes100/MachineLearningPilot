c1e74f46c0152019f5554b5dcad77ed0fc9c86c4
jeronstone <jstone14@terpmail.umd.edu>
2023-01-31 12:05:43 -0500

    Update docker_dli_setup.sh

--- Thumbs Activity/Docker Setup/docker_dli_setup.sh
+++ Thumbs Activity/Docker Setup/docker_dli_setup.sh
@@ -8,6 +8,10 @@ echo "sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML-exp
 echo "sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML-exp-enes100/sec-0101/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1" > docker_dli_run_sec0101.sh
 echo "sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML-exp-enes100/sec-0201/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1" > docker_dli_run_sec0201.sh
 echo "sudo docker run --runtime nvidia -it --rm --network host --volume ~/ML-exp-enes100/sec-0501/:/nvdli-nano/ --device /dev/video0 nvcr.io/nvidia/dli/dli-nano-ai:v2.0.2-r32.7.1" > docker_dli_run_sec0501.sh
+chmod +x docker_dli_run_allsec.sh
+chmod +x docker_dli_run_sec0101.sh
+chmod +x docker_dli_run_sec0201.sh
+chmod +x docker_dli_run_sec0501.sh
 cp ./thumbs_activity_src/thumbs.ipynb ML-exp-enes100/sec-0101/
 cp ./thumbs_activity_src/thumbs.ipynb ML-exp-enes100/sec-0201/
 cp ./thumbs_activity_src/thumbs.ipynb ML-exp-enes100/sec-0501/
