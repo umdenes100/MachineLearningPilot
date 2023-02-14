2dada44fe737ef27e12ea60b6383de89d1c436de
jeronstone <jstone14@terpmail.umd.edu>
2023-02-14 16:02:59 -0500

    update jetson client

--- JetsonClient/JetsonWSClient.py
+++ JetsonClient/JetsonWSClient.py
@@ -3,28 +3,32 @@ import json
 
 
 class JetsonClient:
-
-    def __init__(self, handler, team_name):
-        self.team_name = team_name
-        self.handler = handler
-        self.ws = websocket.WebSocketApp("ws://192.168.1.2:9000/", on_message=self.on_message)
-        # self.ws.connect("ws://192.168.1.2:9000/")
-        # noinspection PyTypeChecker
-        self.ws.send(json.loads({
-            "name": team_name
-        }))
-
-    def on_message(self, message):
+    def on_message(self, _, message):
+        print(message)
         message = json.loads(message)
         if message['op'] == 'prediction':
             # TODO add hex decoder
             results = self.handler(message['image'])
-            # noinspection PyTypeChecker
-            self.ws.send(json.loads({
+            self.ws.send(json.dumps({
                 "op": "prediction_results",
-                "prediction": results,
-                "team_name": self.team_name
+                "prediction": results
             }))
+        
+        
+    def on_open(self, _):
+        print("Opened!")
+        self.ws.send(json.dumps({
+            "op": "begin",
+            "teamName": self.team_name
+        }))
+        
+        
+    def on_error(self, _, error):
+        print(error)
+        
 
-    def start(self):
-        self.ws.run_forever()
+    def __init__(self, handler, team_name):
+        self.team_name = team_name
+        self.handler = handler
+        # websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp("ws://192.168.1.2:7756", on_message=self.on_message, on_open=self.on_open, on_error=self.on_error)
--- JetsonClient/example.py
+++ JetsonClient/example.py
@@ -3,13 +3,14 @@ import torchvision
 from utils import preprocess
 import torch.nn.functional as F
 import JetsonWSClient
+import threading
 
-categories = ['l', 't', 'f']
+categories = ['l', 'tf']
 device = torch.device('cuda')
 model = torchvision.models.resnet18(pretrained=True)
 model.fc = torch.nn.Linear(512, 2)
 model = model.to(device)
-model.load_state_dict(torch.load('model_path'))
+model.load_state_dict(torch.load('../data/dirt-mission/Josh_LTF/model.pth'))
 
 
 def handler(image):
@@ -18,6 +19,7 @@ def handler(image):
     output = F.softmax(output, dim=1).detach().cpu().numpy().flatten()
     return categories[output.argmax()]
 
-
 client = JetsonWSClient.JetsonClient(handler, 'Team Bofa')
-client.start()
+
+# threading.Thread(target=client.ws.run_forever, name="WS Thread",daemon=False).start()
+client.ws.run_forever()
